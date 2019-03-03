daf83dc2aaf27bf4ef48eb1d8395d4da1d5eea46
Max <max.oppor1@gmail.com>
2019-03-03 14:36:58 -0600

    Updates

---
+++ precache-manifest.fcc78e4f255a7e095fcdc5ec3f44fb69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56f5263e5df59d52a4d",
+    "url": "/React-Tetris/static/css/main.6b4d8ce9.chunk.css"
+  },
+  {
+    "revision": "f56f5263e5df59d52a4d",
+    "url": "/React-Tetris/static/js/main.f56f5263.chunk.js"
+  },
+  {
+    "revision": "a116670bb6f7562f557e",
+    "url": "/React-Tetris/static/js/runtime~main.a116670b.js"
+  },
+  {
+    "revision": "fbbeeff632fdfc8a05ec",
+    "url": "/React-Tetris/static/js/2.fbbeeff6.chunk.js"
+  },
+  {
+    "revision": "eee0d88152908ec3a0743f6fc4e99ad1",
+    "url": "/React-Tetris/index.html"
+  }
+];
